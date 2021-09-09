2f2ffd5729a7c25c8862cf8e947ca6033a40b297
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-09 14:55:39 -0500

    Added hours

--- Assignment3/Activity 2.py
+++ Assignment3/Activity 2.py
@@ -1,5 +1,6 @@
-#A basic program that tells you how old you are in months, days, and seconds
+# A simple program that tells you your age in months days and seconds
 age = float(input())
 print(str(age * 12) + " months old")
 print(str(age * 365) + " days old")
+print(str(age * (365 * 24)) + " hours old")
 print(str(age * (365 * (24 * (60 * 60)))) + " Seconds old")
