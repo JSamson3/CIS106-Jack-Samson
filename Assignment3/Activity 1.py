5bbc41ed404f788d849b569277ce3922bdf9ea5f
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-09 15:18:56 -0500

    Your tests are the worst

--- Assignment3/Activity 1.py
+++ Assignment3/Activity 1.py
@@ -1,8 +1,11 @@
 # A simple program that tells you your weekly monthly and yearly wage
+weeksinaweek = 1
 weeksinamonth = 4
 weeksinayear = 52
 hours = float(input())
 rate = float(input())
+print("Hours =" + str(hours))
+print("Rate =" + str(rate))
 print("Weekly " + str(hours * rate))
 print("Monthly " + str(hours * rate * weeksinamonth))
 print("Yearly " + str(hours * rate * weeksinayear))
--- Assignment3/Activity 2.py
+++ Assignment3/Activity 2.py
@@ -1,9 +1,11 @@
 # A simple program that tells you your age in months days and seconds
+yearsinayear = 1
 monthsinayear = 12
 daysinayear = 365
 hoursinayear = 365 * 24
 secondsinayear = 365 * (24 * (60 * 60))
 age = float(input())
+print(str(age * yearsinayear) + " Years old")
 print(str(age * monthsinayear) + " months old")
 print(str(age * daysinayear) + " days old")
 print(str(age * hoursinayear) + " hours old")
