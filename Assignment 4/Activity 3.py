09b8e62038a1e959ad8d9898ffe452fba12620b7
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-13 12:43:50 -0500

    finished

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -3,9 +3,15 @@ mile = 1
 yards = 1760
 feet = yards * 3
 inches = feet * 12
+kilometer = mile * 1.609344
+meter = kilometer * 1000
+centimeter = meter * 100
 print("input amount of miles")
 distance = float(input())
 print(str(distance*mile) + "miles long")
 print(str(distance*yards)+"yards long")
 print(str(distance*feet)+"feet long")
 print(str(distance*inches)+"inches long")
+print(str(distance*kilometer)+"kilometers long")
+print(str(distance*meter)+"meters long")
+print(str(distance*centimeter)+"centimeters long")
