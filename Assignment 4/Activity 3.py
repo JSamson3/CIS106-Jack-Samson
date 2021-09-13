8ff153536d477ae1f6b8f6e4cc53c5c3b8689cab
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-13 11:26:57 -0500

    new folder and activity 3 finished

---
+++ Assignment 4/Activity 3.py
@@ -0,0 +1,11 @@
+#a simple program designed to convwert however many miles into other units of mesurement
+mile = 1
+yards = 1760
+feet = yards * 3
+inches = feet * 12
+print("input amount of miles")
+distance = float(input())
+print(str(distance*mile) + "miles long")
+print(str(distance*yards)+"yards long")
+print(str(distance*feet)+"feet long")
+print(str(distance*inches)+"inches long")
