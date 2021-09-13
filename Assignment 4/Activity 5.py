d41e4eb97ec55f449bff62729ba58f21d7f69ab3
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-13 13:40:28 -0500

    uploaded for draft

---
+++ Assignment 4/Activity 5.py
@@ -0,0 +1,11 @@
+#a simple program to calculate square yards
+print("input X feet")
+XFeet = float(input())
+print("input Y feet")
+YFeet = float(input())
+squarefootage=XFeet*YFeet
+squareyards=squarefootage/9
+print(str(XFeet) + "X feet")
+print(str(YFeet) + "Y feet")
+print(str (squarefootage) +"square footage")
+print(str (squareyards)+"square yards")
