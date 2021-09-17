ea05697996ead3ad97d251f89bbce3cc488fb0aa
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-16 20:05:28 -0500

    Update Activity 5.py

--- Assignment 4/Activity 5.py
+++ Assignment 4/Activity 5.py
@@ -8,8 +8,8 @@ y_feet = float(input())
 squarefootage=x_feet*y_feet
 squareyards=squarefootage/9
 
-print(str(x_feet) + "X feet")
-print(str(y_feet) + "Y feet")
+print(str(x_feet) + "length")
+print(str(y_feet) + "width")
 
 print(str (squarefootage) + "square footage")
 print(str (squareyards) + "square yards")
