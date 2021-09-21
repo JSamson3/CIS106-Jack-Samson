27cbf19aac0250849a1c60fc009371e0b122e4e0
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-21 12:55:58 -0500

    i hate these tests

--- Assignment 5/Activity 7.py
+++ Assignment 5/Activity 7.py
@@ -1,4 +1,9 @@
 #A simple program that tells you your dogs age in dog years
-name = input()
-age = int(input())
-print(name + " is " + str(age * 7) + " years old in dog years.")
+def age_func():
+    print(str(name) + " is " + str(dogyears) + " years old in dog years")
+print("input dog name")
+name = str(input())
+print("input dog age")
+age = float(input())
+dogyears = (age*7)
+age_func()
