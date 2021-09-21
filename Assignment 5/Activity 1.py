9419b6ba7f01df20647c1dc97cebd92a8a6c834c
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-21 14:24:56 -0500

    Update Activity 1.py

--- Assignment 5/Activity 1.py
+++ Assignment 5/Activity 1.py
@@ -1,15 +1,18 @@
-# A simple program that tells you your weekly monthly and yearly wage
+#A simple program that tells you your weekly monthly and yearly wage
 def pay_func():
     print("hours =" + str(hours))
     print("rate =" + str(rate))
     print("weekly " + str(hours * rate))
-    print("monthly " + str(hours * rate * weeksinamonth))
-    print("yearly " + str(hours * rate * weeksinayear))
-weeksinaweek = 1
-weeksinamonth = 4
-weeksinayear = 52
+    print("monthly " + str(hours * rate * weeks_in_a_month))
+    print("yearly " + str(hours * rate * weeks_in_a_year))
+    
+weeks_in_a_week = 1
+weeks_in_a_month = 4
+weeks_in_a_year = 52
+
 print("input weekly hours")
 hours = float(input())
 print("input hourly rate")
 rate = float(input())
+
 pay_func()
