2bc920d59c3294639fd5ada30ced2752b8a81afd
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-22 10:27:22 -0500

    Add files via upload

--- Assignment 5/Activity 1.py
+++ Assignment 5/Activity 1.py
@@ -3,7 +3,6 @@ def input_func1():
     print("input weekly hours")
     global hours
     hours = float(input())
-    
 
 def input_func2():
     print("input hourly rate")
@@ -14,16 +13,19 @@ def input_func2():
 def pay_func():
     input_func1()
     input_func2()
-    print("hours =" + str(hours))
-    print("rate =" + str(rate))
+    print(hours)
+    print(rate)
+    print("Hours =" + str(hours))
+    print("Rate =" + str(rate))
     print("Weekly " + str(hours * rate))
-    print("Monthly " + str(hours * rate * weeks_in_a_month))
-    print("Yearly " + str(hours * rate * weeks_in_a_year))
+    print("Monthly " + str(hours * rate * weeksinamonth))
+    print("Yearly " + str(hours * rate * weeksinayear))
 
 
-weeks_in_a_week = 1
-weeks_in_a_month = 4
-weeks_in_a_year = 52
+
+weeksinaweek = 1
+weeksinamonth = 4
+weeksinayear = 52
 
 
 pay_func()
