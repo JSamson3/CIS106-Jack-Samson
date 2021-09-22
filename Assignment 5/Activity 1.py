6370c1d7000e6662de58efdefaf5e9ec296d8067
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-22 09:54:02 -0500

    Update Activity 1.py

--- Assignment 5/Activity 1.py
+++ Assignment 5/Activity 1.py
@@ -7,12 +7,10 @@ def pay_func():
     print("Yearly " + str(hours * rate * weeksinayear))
 
 
-    
 weeksinaweek = 1
 weeksinamonth = 4
 weeksinayear = 52
-print("input weekly hours")
+
 hours = float(input())
-print("input hourly rate")
 rate = float(input())
 pay_func()
