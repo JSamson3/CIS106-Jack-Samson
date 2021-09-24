819154431b2d5e49a583ea8c362a5e5fe615d6c8
Dave Braunschweig <dbraunsc@harpercollege.edu>
2021-09-24 12:07:43 -0500

    Assignment 5 corrections

--- Assignment 5/Activity 7.py
+++ Assignment 5/Activity 7.py
@@ -1,9 +1,26 @@
-#A simple program that tells you your dogs age in dog years
-def age_func():
-    print(str(name) + " is " + str(dogyears) + " years old in dog years")
-print("input dog name")
-name = str(input())
-print("input dog age")
-age = float(input())
-dogyears = (age*7)
-age_func()
+def calculateDogYears(age):
+    dogYears = age * 7
+    
+    return dogYears
+
+def displayResults(name, dogYears):
+    print(name + " is " + str(dogYears) + " in dog years")
+
+def getAge():
+    print("Enter dog's age:")
+    age = int(input())
+    
+    return age
+
+def getName():
+    print("Enter dog's name:")
+    name = input()
+    
+    return name
+
+# Main
+# A simple program that tells you your dogs age in dog years
+name = getName()
+age = getAge()
+dogYears = calculateDogYears(age)
+displayResults(name, dogYears)
