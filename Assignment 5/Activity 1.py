01fa190c57369c829fef494f366900aea34e6832
Dave Braunschweig <dbraunsc@harpercollege.edu>
2021-09-27 11:11:29 -0500

    Assignment 5 corrections

--- Assignment 5/Activity 1.py
+++ Assignment 5/Activity 1.py
@@ -1,59 +1,61 @@
 # A simple program that tells you your weekly monthly and yearly wage
-def input_func1():
+
+WEEKS_IN_A_MONTH = 4
+WEEKS_IN_A_YEAR = 52
+
+
+def input_hours():
     print("input weekly hours")
     hours = float(input())
     return hours
 
 
-def input_func2():
+def input_rate():
     print("input hourly rate")
     rate = float(input())
     return rate
 
 
-def processing_func1(hours, rate):
+def process_weekly(hours, rate):
     hourlyrate = hours * rate
     return hourlyrate
 
 
-def print_func1(hours, rate, hourlyrate):
+def print_weekly(hours, rate, hourlyrate):
     print("hours =" + str(hours))
     print("rate =" + str(rate))
     print("Weekly " + str(hourlyrate))
 
 
-def processing_func2(hours, rate):
-    monthlyrate = hours * rate * weeksinamonth
-    return monthlyrate
+def process_monthly(hours, rate):
+    monthly_rate = hours * rate * WEEKS_IN_A_MONTH
+    return monthly_rate
 
 
-def print_func2(monthlyrate):
-    print("Monthly " + str(monthlyrate))
+def print_monthly(monthly_rate):
+    print("Monthly " + str(monthly_rate))
 
 
-def processing_func3(hours, rate):
-    yearlyrate = (hours * rate * weeksinayear)
-    return yearlyrate
+def process_yearly(hours, rate):
+    yearly_rate = (hours * rate * WEEKS_IN_A_YEAR)
+    return yearly_rate
 
 
-def print_func3(yearlyrate):
-    print("Yearly " + str(yearlyrate))
-
-
-weeksinaweek = 1
-weeksinamonth = 4
-weeksinayear = 52
+def print_yearly(yearly_rate):
+    print("Yearly " + str(yearly_rate))
 
 
 def main():
-    hours = input_func1()
-    rate = input_func2()
-    hourlyrate = processing_func1(hours, rate)
-    monthlyrate = processing_func2(hours, rate)
-    yearlyrate = processing_func3(hours, rate)
-    print_func1(hours, rate, hourlyrate)
-    print_func2(monthlyrate)
-    print_func3(yearlyrate)
+    hours = input_hours()
+    rate = input_rate()
+
+    hourly_rate = process_weekly(hours, rate)
+    monthly_rate = process_monthly(hours, rate)
+    yearly_rate = process_yearly(hours, rate)
+    
+    print_weekly(hours, rate, hourly_rate)
+    print_monthly(monthly_rate)
+    print_yearly(yearly_rate)
 
 
 main()
