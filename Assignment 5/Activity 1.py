a45c79b37d1be743d7be069d85739994d12bc9a0
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-09-27 10:29:10 -0500

    ever consider a career change?

--- Assignment 5/Activity 1.py
+++ Assignment 5/Activity 1.py
@@ -1,30 +1,43 @@
 # A simple program that tells you your weekly monthly and yearly wage
 def input_func1():
     print("input weekly hours")
-    global hours
     hours = float(input())
     return hours
 
 
 def input_func2():
     print("input hourly rate")
-    global rate
     rate = float(input())
     return rate
 
 
-def processing_func1():
+def processing_func1(hours , rate):
+    hourlyrate = hours * rate
+    return hourlyrate
+
+
+def print_func1(hours , rate , hourlyrate):
     print("hours =" + str(hours))
     print("rate =" + str(rate))
-    print("Weekly " + str(hours * rate))
+    print("Weekly " + str(hourlyrate))
 
 
-def processing_func2():
-    print("Monthly " + str(hours * rate * weeksinamonth))
+def processing_func2(hours , rate):
+    monthlyrate = hours * rate * weeksinamonth
+    return monthlyrate
 
 
-def processing_func3():
-    print("Yearly " + str(hours * rate * weeksinayear))
+def print_func2(monthlyrate):
+    print("Monthly " + str(monthlyrate))
+
+
+def processing_func3(hours , rate):
+    yearlyrate = (hours * rate * weeksinayear)
+    return yearlyrate
+
+
+def print_func3(yearlyrate):
+    print("Yearly " + str(yearlyrate))
 
 
 weeksinaweek = 1
@@ -33,11 +46,14 @@ weeksinayear = 52
 
 
 def main():
-    input_func1()
-    input_func2()
-    processing_func1()
-    processing_func2()
-    processing_func3()
+    hours = input_func1()
+    rate = input_func2()
+    hourlyrate = processing_func1(hours , rate)
+    monthlyrate = processing_func2(hours , rate)
+    yearlyrate = processing_func3(hours , rate)
+    print_func1(hours , rate , hourlyrate)
+    print_func2(monthlyrate)
+    print_func3(yearlyrate)
 
 
 main()
