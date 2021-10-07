3bb0ff3b0e9e20e079d32ba101e7a3efff1c57cb
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-10-07 13:35:42 -0500

    Add files via upload

--- Assignment 7/Activity 2.py
+++ Assignment 7/Activity 2.py
@@ -3,7 +3,7 @@ yearsinayear = 1
 monthsinayear = 12
 daysinayear = 365
 hoursinayear = 365 * 24
-secondsinayear = 365 * (24 * (60 * 60))
+secondsinayear = 31536000
 
 
 def input_func():
@@ -18,23 +18,47 @@ def what_age_func():
     return response
 
 
-def output_func(response, age):
+def months_processing(age):
+    monthsold = age * monthsinayear
+    return monthsold
+
+
+def days_processing(age):
+    daysold = age * daysinayear
+    return daysold
+
+
+def hour_processing(age):
+    hoursold = age * hoursinayear
+    return hoursold
+
+
+def seconds_processing(age):
+    secondsold = age * secondsinayear
+    return secondsold
+
+
+def output_func(response, age, monthsold, daysold, hoursold, secondsold):
     if response == "m" or response == "M":
-        print(str(monthsinayear * age) + "Months old")
+        print(str(monthsold) + "Months old")
     elif response == "d" or response == "D":
-        print(str(daysinayear * age) + "Days old")
+        print(str(daysold) + "Days old")
     elif response == "h" or response == "H":
-        print(str(age * hoursinayear) + "Hours old")
+        print(str(hoursold) + "Hours old")
     elif response == "s" or response == "S":
-        print(str(age * secondsinayear) + "Seconds old")
+        print(str(secondsold) + "Seconds old")
     else:
         print("invalid selection")
 
 
 def main():
-    response = what_age_func()
     age = input_func()
-    output_func(response, age)
+    response = what_age_func()
+    monthsold = months_processing(age)
+    daysold = days_processing(age)
+    hoursold = hour_processing(age)
+    secondsold = seconds_processing(age)
+    output_func(response, age, monthsold, daysold, hoursold, secondsold)
 
 
 main()
