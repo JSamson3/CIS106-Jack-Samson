d13c9afe449851081230dd761a3742edc2f6b18c
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-10-09 11:21:32 -0500

    Add files via upload

---
+++ Assignment8/Activity 1.py
@@ -0,0 +1,27 @@
+def input_func1():
+    print("Input number")
+    input_number = float(input())
+    return input_number
+
+
+def input_func2():
+    print("How many times?")
+    input_times = float(input())
+    return input_times
+
+
+def the_loop(input_number, input_times):
+    number_go_up = 1
+    while input_times > 0:
+        print(str(input_number) + " * " + str(number_go_up) + "=" + (str(input_number*number_go_up)))
+        number_go_up = number_go_up + 1
+        input_times = input_times - 1
+
+
+def main():
+    input_number = input_func1()
+    input_times = input_func2()
+    the_loop(input_number, input_times)
+
+
+main()
