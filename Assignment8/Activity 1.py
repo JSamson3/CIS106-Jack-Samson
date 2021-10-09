8809dbbb9478258ec7b983f939a1e2c08988cac8
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-10-09 12:57:30 -0500

    Add files via upload

--- Assignment8/Activity 1.py
+++ Assignment8/Activity 1.py
@@ -10,18 +10,15 @@ def input_func2():
     return input_times
 
 
-
-
-
 def the_loop(input_number, input_times):
     number_go_up = 1
     while input_times > 0:
 
 
         def processing_func(input_number, input_times, number_go_up):
-            print(str(input_number) + " * " + str(number_go_up) + " = " + (str(input_number*number_go_up)))
+            print(str(number_go_up) + " * " + str(input_number) + " = " + (str(input_number*number_go_up)))
 
-            
+
         processing_func(number_go_up, input_times, input_number)
         number_go_up = number_go_up + 1
         input_times = input_times - 1
