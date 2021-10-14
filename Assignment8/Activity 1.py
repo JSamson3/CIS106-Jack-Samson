62638e496e1ec9286986fcfc140d2163f9644d28
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-10-14 13:39:17 -0500

    Add files via upload

--- Assignment8/Activity 1.py
+++ Assignment8/Activity 1.py
@@ -1,4 +1,7 @@
 #A simple program that multiplies a given number a number of times
+number_go_up = 1
+
+
 def input_func1():
     print("Input number")
     input_number = int(input())
@@ -11,18 +14,19 @@ def input_func2():
     return input_times
 
 
+def processing_func(input_number, input_times, number_go_up):
+    new_print = input_number * number_go_up
+    return new_print
+
+
 def the_loop(input_number, input_times):
     number_go_up = 1
     while input_times > 0:
-
-
-        def processing_func(input_number, input_times, number_go_up):
-            print(str(number_go_up) + " * " + str(input_number) + " = " + (str(input_number*number_go_up)))
-
-
-        processing_func(number_go_up, input_times, input_number)
-        number_go_up = number_go_up + 1
         input_times = input_times - 1
+        new_print = processing_func(input_number, input_times, number_go_up)
+        print(str(input_number) + " * " + str(number_go_up) + " = " + str(new_print))
+        number_go_up = number_go_up + 1
+
 
 
 def main():
