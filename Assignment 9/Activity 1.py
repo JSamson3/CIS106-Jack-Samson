f0a5361d90edf23af2c206742a8331e64820032b
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-10-18 22:19:57 -0500

    Reupload cause you seemed to use a earlier version of the files

---
+++ Assignment 9/Activity 1.py
@@ -0,0 +1,37 @@
+# A simple program that multiplies a given number a number of times
+number_up = 1
+
+
+def input_func1():
+    print("Input number")
+    input_number = int(input())
+    return input_number
+
+
+def input_func2():
+    print("How many times?")
+    input_times = int(input())
+    return input_times
+
+
+def processing_func(input_number, input_times, number_up):
+    output = input_number * number_up
+    return output
+
+
+def the_loop(input_number, input_times):
+    number_up = 1
+    while input_times > 0:
+        input_times = input_times - 1
+        output = processing_func(input_number, input_times, number_up)
+        print(str(input_number) + " * " + str(number_up) + " = " + str(output))
+        number_up = number_up + 1
+
+
+def main():
+    input_number = input_func1()
+    input_times = input_func2()
+    the_loop(input_number, input_times)
+
+
+main()
