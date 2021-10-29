6956bf0b2e54b16090f03c83f4f561c369e84fd5
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-10-28 20:09:37 -0500

    Add files via upload

--- Assignment 10/Nested Activity 1.py
+++ Assignment 10/Nested Activity 1.py
@@ -1,6 +1,26 @@
-input1 = int(input())
-input2 = int(input())
-for i in range(1 , input1+1):
-    for j in range (1 , input2+1):
-        print ((i*j), end="   ")
-    print()
+def get_number1():
+    print("input 1")
+    input1 = int(input())
+    return input1
+
+
+def get_number2():
+    print("input 2")
+    input2 = int(input())
+    return input2
+
+
+def print_table(input1, input2):
+    for i in range(1, input1 + 1):
+        for j in range(1, input2 + 1):
+            print((i * j), end = " ")
+        print()
+
+
+def main():
+    input1 = get_number1()
+    input2 = get_number2()
+    print_table(input1, input2)
+
+
+main()
