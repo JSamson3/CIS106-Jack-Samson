ec8d74de1352d307894ba10b74b7da393fb06eef
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-10-28 22:54:15 -0500

    Add files via upload

--- Assignment 10/Nested Activity 1.py
+++ Assignment 10/Nested Activity 1.py
@@ -3,32 +3,32 @@
 
 def get_starting():
     print("starting?")
-    first_input = int(input())
-    return first_input
+    starting_input = int(input())
+    return starting_input
 
 
 def get_ending():
     print("ending?")
-    second_input = int(input())
-    return second_input
+    ending_input = int(input())
+    return ending_input
 
 
-def print_table(first_input, second_input):
+def print_table(starting_input, ending_input):
     print((" "), end=" ")
-    for c in range(first_input, second_input + 1):
+    for c in range(starting_input, ending_input + 1):
         print((c), end=" ")
     print()
-    for a in range(first_input, second_input + 1):
+    for a in range(starting_input, ending_input + 1):
         print((a), end=" ")
-        for b in range(first_input, second_input + 1):
+        for b in range(starting_input, ending_input + 1):
             print((a * b), end=" ")
         print()
 
 
 def main():
-    first_input = get_starting()
-    second_input = get_ending()
-    print_table(first_input, second_input)
+    starting_input = get_starting()
+    ending_input = get_ending()
+    print_table(starting_input, ending_input)
 
 
 main()
