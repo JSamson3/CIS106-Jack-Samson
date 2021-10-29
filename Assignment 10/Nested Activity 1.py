8a3865f870cdc8afdef28e10cc1c5e575ae906bd
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-10-28 22:24:09 -0500

    Add files via upload

--- Assignment 10/Nested Activity 1.py
+++ Assignment 10/Nested Activity 1.py
@@ -1,18 +1,23 @@
+# A simple program that gives you a multiplication table
+
+
 def get_number1():
-    print("input 1")
+    print("first?")
     input1 = int(input())
     return input1
 
 
 def get_number2():
-    print("input 2")
+    print("second?")
     input2 = int(input())
     return input2
 
 
 def print_table(input1, input2):
-    for a in range(input1, input2+1):
-        for b in range(input1, input2+1):
+    print((""), end = "")
+    for a in range(input1, input2 + 1):
+        print((a), end = " ")
+        for b in range(input1, input2 + 1):
             print((a * b), end=" ")
         print()
 
@@ -21,6 +26,7 @@ def main():
     input1 = get_number1()
     input2 = get_number2()
     print_table(input1, input2)
+    print("end")
 
 
 main()
