99857d7ee15f58b643833f0533fbfd830f657f88
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-11-04 21:56:35 -0500

    Add files via upload

--- Assignment 11/Defined Activity 1.py
+++ Assignment 11/Defined Activity 1.py
@@ -5,7 +5,7 @@ def get_month():
     print("enter month number")
     month_input = int(input()) - 1
     if 11 < month_input or month_input <= -1:
-        print("invalid")
+        print("invalid month")
         quit()
     return month_input
 
@@ -13,8 +13,8 @@ def get_month():
 def get_year():
     print("enter year")
     year_input = int(input())
-    if year_input < 0:
-        print("invalid")
+    if year_input <= -1:
+        print("invalid year")
         quit()
     return year_input
 
