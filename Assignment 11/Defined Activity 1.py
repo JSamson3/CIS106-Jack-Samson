1f30bf38c4baa9d0c1fa840b9874fad417cd2fd7
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-11-04 21:33:04 -0500

    Add files via upload

--- Assignment 11/Defined Activity 1.py
+++ Assignment 11/Defined Activity 1.py
@@ -20,41 +20,41 @@ def get_year():
 
 
 def process_leap(year_input): 
-    days_in_year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 30]   
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 30]   
     if year_input % 4 == 0 and year_input % 100 != 0:
-        days_in_year[1] = 29
+        days[1] = 29
     elif year_input % 400 == 0:
-        days_in_year[1] = 29
+        days[1] = 29
     else:
-        days_in_year[1] = 28
-    return days_in_year
+        days[1] = 28
+    return days
 
 
-def display_array(month_input, days_in_year, year_input):
-    months_in_year = ["January", "Febuary", "March", "April", "May",
+def display_array(month_input, days, year_input):
+    months = ["January", "Febuary", "March", "April", "May",
      "June", "July", "August", "September",
       "October", "November", "December"]
-    print(months_in_year[month_input], year_input, "has", days_in_year[month_input], "days")
+    print(months[month_input], year_input, "has", days[month_input], "days")
 
 
 def loop_function(month_input, year_input):
     while 11 > month_input > 0 or year_input > 0:
-        month_input = get_month()
         year_input = get_year()
+        month_input = get_month()
         if month_input >= 11 or month_input <= 0:
             break
         if year_input >= 0:
             break
-        days_in_year = process_leap(year_input)
-        display_array(month_input, days_in_year, year_input)
+        days = process_leap(year_input)
+        display_array(month_input, days, year_input)
 
 
 
 def main():
     year_input = get_year()
     month_input = get_month()
-    days_in_year = process_leap(year_input)
-    display_array(month_input, days_in_year, year_input)
+    days = process_leap(year_input)
+    display_array(month_input, days, year_input)
     loop_function(month_input, year_input)
 
 
