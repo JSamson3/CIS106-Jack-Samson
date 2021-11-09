9899bfa8dcc26fe92a934215c5c220ab0a92acdc
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-11-08 21:52:43 -0600

    Create Activity 1.py

---
+++ Assignment 12/Activity 1.py
@@ -0,0 +1,52 @@
+# A program that calculates averages, max, and min
+
+
+def get_times():
+    print("How many scores?")
+    input_times = int(input())
+    return input_times
+
+
+def process_inputs(input_times):
+    scores = []
+    for count in range(0, input_times):
+        print(scores)
+        print("input score")
+        append(scores) = int(input())
+    print(scores)
+    return scores
+
+
+def calculate_max(scores):
+    print("high", max(scores))
+
+
+def calculate_min(scores):
+    print("low", min(scores))
+
+
+def calculate_average(scores):
+    output = 0
+    for x in scores:
+        output = output + x
+    number = len(scores)
+    average = output / number
+    return average
+
+
+def print_average(average):
+    print("average is", average)
+    
+
+def main():
+    input_times = get_times()
+    scores = process_inputs(input_times)
+
+    calculate_max(scores)
+    calculate_min(scores)
+    average = calculate_average(scores)
+    
+    print_average(average)
+
+
+main()
