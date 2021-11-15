2a8452dc787c55e47f145f9a9d58e4b35cf8a82c
JSamson3 <89407580+JSamson3@users.noreply.github.com>
2021-11-15 12:22:12 -0600

    Create Activity 1.py

---
+++ Assignment 13/Activity 1.py
@@ -0,0 +1,23 @@
+#A simple program that asks for your name
+def get_first_name():
+    print("first name")
+    first = input(str())
+    return first
+
+
+def get_last_name():
+    print("last name")
+    last = input(str())
+    return last
+
+
+def display_answer(first, last):
+    print(last, first[0:1],".")
+
+
+def main():
+    first = get_first_name()
+    last = get_last_name()
+    display_answer(first, last)
+
+main()
