29fe2e46e3d481386348823e5dd717484d63188d
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-11-15 19:52:19 -0600

    better answer

--- Assignment 13/Activity 1.py
+++ Assignment 13/Activity 1.py
@@ -1,18 +1,18 @@
 #A simple program that asks for your name
 def get_first_name():
-    print("first name")
+    print("input first name")
     first = input(str())
     return first
 
 
 def get_last_name():
-    print("last name")
+    print("input last name")
     last = input(str())
     return last
 
 
 def display_answer(first, last):
-    print(last, first[0:1],".")
+    print(last, first[0:1] + ".")
 
 
 def main():
