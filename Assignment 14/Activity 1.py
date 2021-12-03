338b25c7f3899c951e93bc86cdf3d1c9a7f0dcf1
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-02 22:08:55 -0600

    aaaaaaa

--- Assignment 14/Activity 1.py
+++ Assignment 14/Activity 1.py
@@ -6,11 +6,44 @@ from os import read
 
 def read_file():
     f = open("Assignment 14\scores.txt")
-    print(f.read().strip())
+    file = f.read().replace("\n", ",").split(",")
+    f.close()
+    print(file)
+    return file
+
+
+def get_max(file):
+    return max(file)
+    
+
+def get_min(file):
+    return min(file)
+
+
+def get_average(file):
+    output = 0
+    for x in file:
+        output = output + x
+    number = len(file)
+    average = output / ((number - 2 ) / 2)
+    return average
+
+
+def print_answer(average, low, high, file):
+    print("high", high)
+    print("low", low)
+    print("average is", average)
+    print("scores:", file)
+
 
 
 def main():
-    read_file()
+    file = read_file()
+    high = get_max(file)
+    low = get_min(file)
+    average = get_average(file)
+    print_answer(high, low, average, file)
+
 
 
 main()
