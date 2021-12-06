1ba92424a64aa659d51ebe5705de4536fd9c4c42
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-06 16:37:35 -0600

    sadfjl;dskfljasklfdjadsfjpoew

--- Assignment 14/Activity 3.py
+++ Assignment 14/Activity 3.py
@@ -1,21 +1,28 @@
-# Fuck you
+# A program that pulls out tags from html and txt files
 
-
-def input_function():
+def open_file():
     print("input")
-    name = str(input())
-    if name.find(".html") == -1: 
-        while name.find(".html") == -1:
-            print("input valid name")
-            name = str(input())
-        return name
-    if name.find(".txt") == -1:
-        while name.find(".txt") == -1:
-            print("input valid name")
-            name = str(input())
-    else:
-        return name
+    text = open(str(input()))
+    file = text.read()
+    print(file)
+    return file
 
 
-def open_file(name):
-    open(name , "at")
+def parse_file1(file):
+    tags = []
+    if file.find("<h1>") >= 0:
+        file.split("<h1>")
+        file.split("</h1>")
+        tags.append("<h1>")
+        tags.append("</h1>")
+    print(tags)
+    return tags
+
+
+
+def main():
+    file = open_file()
+    tags = parse_file1(file)
+
+
+main()
