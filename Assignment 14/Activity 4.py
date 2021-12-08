4eb4d1edff3bf032281f2fcdb49a4c3f27f5c8d3
Dave Braunschweig <dbraunsc@harpercollege.edu>
2021-12-08 09:28:40 -0600

    Assignment 14 corrections

--- Assignment 14/Activity 4.py
+++ Assignment 14/Activity 4.py
@@ -7,33 +7,25 @@ def read_file():
     return text
 
 
-def output_function(text):
+def process_text(text):
     for index in range(len(text)):
-        if index < 4:
-            if index % 4 == 0:
-                print(text[index].strip().split()[1], end=", ")
-                print(text[index].strip().split()[0], sep=", ", end=", ")
-            if index % 4 == 1:
-                print(text[index].strip(), sep=", ", end=", ")
-            if index % 4 == 2:
-                print(text[index].strip().split(",")[0], sep=", ", end=", ")
-                print(text[index].replace(" ", ", ").split(", ")[1], end=", ")
-                print(text[index].replace(" ", ", ").split(", ")[2], end="")
-        if index >= 4:
-            if index % 4 == 0:
-                print(text[index].strip().split()[1], end=", ")
-                print(text[index].strip().split()[0], sep=", ", end=", ")
-            if index % 4 == 1:
-                print(text[index].strip(), sep=", ", end=", ")
-            if index % 4 == 2:
-                print(text[index].strip().split(",")[0], sep=", ", end=", ")
-                print(text[index].replace(", ", " ").split()[2], end=", ")
-                print(text[index].replace(",", " ").split()[3], end="\n")
+        line = text[index].strip()
+        if index % 4 == 0:
+            firstname = line.split()[0]
+            lastname = line.split()[1]
+        elif index % 4 == 1:
+            address = line
+        elif index % 4 == 2:
+            city = line.split(",")[0]
+            state = line.split(",")[1].split()[0]
+            zip = line.split(",")[1].split()[1]
+            print(f"{lastname}, {firstname}, {address},"
+                f" {city}, {state}, {zip}")
 
 
 def main():
     text = read_file()
-    output_function(text)
+    process_text(text)
 
 
 main()
