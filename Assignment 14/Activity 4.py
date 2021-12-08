ae208b4b9c06a4b1207b44868f480e8e99909169
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-07 23:25:52 -0600

    character limits

--- Assignment 14/Activity 4.py
+++ Assignment 14/Activity 4.py
@@ -17,8 +17,8 @@ def output_function(new):
                 print(new[index].strip(), sep=", ", end=", ")
             if index % 4 == 2:
                 print(new[index].strip().split(",")[0], sep=", ", end=", ")
-                print(new[index].strip().replace(" ", ", ").split(", ")[1], end=", ")
-                print(new[index].strip().replace(" ", ", ").split(", ")[2], end="\n")
+                print(new[index].replace(" ", ", ").split(", ")[1], end=", ")
+                print(new[index].replace(" ", ", ").split(", ")[2], end="")
         if index >= 4:
             if index % 4 == 0:
                 print(new[index].strip().split()[1], end=", ")
@@ -27,8 +27,8 @@ def output_function(new):
                 print(new[index].strip(), sep=", ", end=", ")
             if index % 4 == 2:
                 print(new[index].strip().split(",")[0], sep=", ", end=", ")
-                print(new[index].strip().replace(", ", " ").split()[2], end=", ")
-                print(new[index].strip().replace(",", " ").split()[3], end="\n")
+                print(new[index].replace(", ", " ").split()[2], end=", ")
+                print(new[index].replace(",", " ").split()[3], end="\n")
 
 
 def main():
