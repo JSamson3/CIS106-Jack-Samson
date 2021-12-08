748ff1e2c0fa2cff7f15006cb79b703b8198f6ba
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-07 23:20:24 -0600

    will it work?

--- Assignment 14/Activity 4.py
+++ Assignment 14/Activity 4.py
@@ -2,33 +2,34 @@
 
 def read_file():
     file = open("addresses.txt")
-    text = file.readlines()
+    new = file.readlines()
     file.close()
-    return text
+    return new
 
 
 def main():
-    text = read_file()
-    for index in range(len(text)):
+    new = read_file()
+    for index in range(len(new)):
         if index < 4:
             if index % 4 == 0:
-                print(text[index].strip().split(" ")[1], end=", ")
-                print(text[index].strip().split(" ")[0], sep=", ", end=", ")
+                print(new[index].strip().split()[1], end=", ")
+                print(new[index].strip().split()[0], sep=", ", end=", ")
             if index % 4 == 1:
-                print(text[index].strip(), sep=", ", end=", ")
+                print(new[index].strip(), sep=", ", end=", ")
             if index % 4 == 2:
-                print(text[index].strip().split(",")[0], sep=", ", end=", ")
-                print(text[index].strip().replace(" ", ", ").split(", ")[1], end=", ")
-                print(text[index].strip().replace(" ", ", ").split(", ")[2], end="\n")
+                print(new[index].strip().split(",")[0], sep=", ", end=", ")
+                print(new[index].strip().replace(" ", ", ").split(", ")[1], end=", ")
+                print(new[index].strip().replace(" ", ", ").split(", ")[2], end="\n")
         if index >= 4:
             if index % 4 == 0:
-                print(text[index].strip().split(" ")[1], text[index].strip().split(" ")[0], sep=", ", end=", ")
+                print(new[index].strip().split()[1], end=", ")
+                print(new[index].strip().split()[0], sep=", ", end=", ")
             if index % 4 == 1:
-                print(text[index].strip(), sep=", ", end=", ")
+                print(new[index].strip(), sep=", ", end=", ")
             if index % 4 == 2:
-                print(text[index].strip().split(",")[0], sep=", ", end=", ")
-                print(text[index].strip().replace(", ", " ").split()[2], end=", ")
-                print(text[index].strip().replace(",", " ").split()[3], end="\n")
+                print(new[index].strip().split(",")[0], sep=", ", end=", ")
+                print(new[index].strip().replace(", ", " ").split()[2], end=", ")
+                print(new[index].strip().replace(",", " ").split()[3], end="\n")
 
 
 main()
