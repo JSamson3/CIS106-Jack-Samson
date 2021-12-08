1cfd992583b81090124eeae836164e755905ec0c
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-07 22:40:34 -0600

    sweet

--- Assignment 14/Activity 4.py
+++ Assignment 14/Activity 4.py
@@ -7,21 +7,6 @@ def read_file():
     return text
 
 
-def get_first(text):
-    firstname = text[0].split(" ")[0]
-    return firstname
-
-
-def get_last(text):
-    lastname = text[0].split(" ")[1]
-    return lastname
-    
-
-def get_firstpost(text):
-    firstpost = text[2].split(" ")
-    return firstpost
-
-
 def main():
     text = read_file()
     for index in range(len(text)):
@@ -30,14 +15,7 @@ def main():
         if index % 4 == 1:
             print(text[index].strip(), sep=", ", end=", ")
         if index % 4 == 2:
-            print(text[index].strip(), sep=", ", end=", ")
-        if index % 4 == 4:
-            print(text[index].strip().split(" ")[0], text[index].strip().split(" ")[1], sep=", ")
-
-    # firstname = get_first(text)
-    # lastname = get_last(text)
-    # firstpost = get_firstpost(text)
-    # print_function(firstname, lastname, secondfirst, secondlast, thirdfirst, thirdlast, text, firstpost, secondpost, thirdpost)
+            print(text[index].strip(), sep=", ", end="\n")
 
 
 main()
