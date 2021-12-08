13f2baaf8fcce68e4d5477c9785dcf0d556e0974
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-07 22:21:21 -0600

    should work

--- Assignment 14/Activity 4.py
+++ Assignment 14/Activity 4.py
@@ -1,7 +1,7 @@
 # A simple program that prints out addresses for who knows what purpose
 
 def read_file():
-    file = open("addresses.txt")
+    file = open("Assignment 14\Addresses.txt")
     text = file.readlines()
     file.close()
     return text
@@ -22,18 +22,17 @@ def get_firstpost(text):
     return firstpost
 
 
-def print_function(firstname, lastname, secondfirst, secondlast, thirdfirst, thirdlast, text, firstpost, secondpost, thirdpost):
-    print(lastname, ",", firstname, ",", text[1], firstpost[0], ",", firstpost[1], ",", firstpost[2])
-    print(secondlast, ",", secondfirst, ",", text[5], secondpost[0], ",", secondpost[1], ",", secondpost[2])
-    print(thirdlast, ",", thirdfirst, ",", text[7], thirdpost[0], ",", thirdpost[1], ",", thirdpost[2])
-
-
 def main():
     text = read_file()
     for index in range(len(text)):
         if index % 4 == 0:
-            print(index, text[index].strip())
-
+            print(text[index].strip().split(" ")[1], text[index].strip().split(" ")[0], sep=", ", end=", ")
+        if index % 4 == 1:
+            print(text[index].strip(), sep=", ", end=", ")
+        if index % 4 == 2:
+            print(text[index].strip(), sep=", ", end=", ")
+        if index % 4 == 4:
+            print(index, text[index].strip(), sep=", ", end="\n")
 
     # firstname = get_first(text)
     # lastname = get_last(text)
