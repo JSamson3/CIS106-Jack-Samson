65d5da297f14235f8484c35e8d825ff0f57cd097
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-07 23:11:19 -0600

    oh yeah

--- Assignment 14/Activity 4.py
+++ Assignment 14/Activity 4.py
@@ -10,12 +10,24 @@ def read_file():
 def main():
     text = read_file()
     for index in range(len(text)):
-        if index % 4 == 0:
-            print(text[index].strip().split(" ")[1], text[index].strip().split(" ")[0], sep=", ", end=", ")
-        if index % 4 == 1:
-            print(text[index].strip(), sep=", ", end=", ")
-        if index % 4 == 2:
-            print(text[index].strip(), sep=", ", end="\n")
+        if index < 4:
+            if index % 4 == 0:
+                print(text[index].strip().split(" ")[1], text[index].strip().split(" ")[0], sep=", ", end=", ")
+            if index % 4 == 1:
+                print(text[index].strip(), sep=", ", end=", ")
+            if index % 4 == 2:
+                print(text[index].strip().split(",")[0], sep=", ", end=", ")
+                print(text[index].strip().replace(" ", ", ").split(", ")[1], end=", ")
+                print(text[index].strip().replace(" ", ", ").split(", ")[2], end="\n")
+        if index >= 4:
+            if index % 4 == 0:
+                print(text[index].strip().split(" ")[1], text[index].strip().split(" ")[0], sep=", ", end=", ")
+            if index % 4 == 1:
+                print(text[index].strip(), sep=", ", end=", ")
+            if index % 4 == 2:
+                print(text[index].strip().split(",")[0], sep=", ", end=", ")
+                print(text[index].strip().replace(", ", " ").split()[2], end=", ")
+                print(text[index].strip().replace(",", " ").split()[3], end="\n")
 
 
 main()
