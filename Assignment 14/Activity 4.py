6e7004cea7e78197c488dde5aca685e61adfa458
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-07 23:27:07 -0600

    character limits?

--- Assignment 14/Activity 4.py
+++ Assignment 14/Activity 4.py
@@ -2,38 +2,38 @@
 
 def read_file():
     file = open("addresses.txt")
-    new = file.readlines()
+    text = file.readlines()
     file.close()
-    return new
+    return text
 
 
-def output_function(new):
-    for index in range(len(new)):
+def output_function(text):
+    for index in range(len(text)):
         if index < 4:
             if index % 4 == 0:
-                print(new[index].strip().split()[1], end=", ")
-                print(new[index].strip().split()[0], sep=", ", end=", ")
+                print(text[index].strip().split()[1], end=", ")
+                print(text[index].strip().split()[0], sep=", ", end=", ")
             if index % 4 == 1:
-                print(new[index].strip(), sep=", ", end=", ")
+                print(text[index].strip(), sep=", ", end=", ")
             if index % 4 == 2:
-                print(new[index].strip().split(",")[0], sep=", ", end=", ")
-                print(new[index].replace(" ", ", ").split(", ")[1], end=", ")
-                print(new[index].replace(" ", ", ").split(", ")[2], end="")
+                print(text[index].strip().split(",")[0], sep=", ", end=", ")
+                print(text[index].replace(" ", ", ").split(", ")[1], end=", ")
+                print(text[index].replace(" ", ", ").split(", ")[2], end="")
         if index >= 4:
             if index % 4 == 0:
-                print(new[index].strip().split()[1], end=", ")
-                print(new[index].strip().split()[0], sep=", ", end=", ")
+                print(text[index].strip().split()[1], end=", ")
+                print(text[index].strip().split()[0], sep=", ", end=", ")
             if index % 4 == 1:
-                print(new[index].strip(), sep=", ", end=", ")
+                print(text[index].strip(), sep=", ", end=", ")
             if index % 4 == 2:
-                print(new[index].strip().split(",")[0], sep=", ", end=", ")
-                print(new[index].replace(", ", " ").split()[2], end=", ")
-                print(new[index].replace(",", " ").split()[3], end="\n")
+                print(text[index].strip().split(",")[0], sep=", ", end=", ")
+                print(text[index].replace(", ", " ").split()[2], end=", ")
+                print(text[index].replace(",", " ").split()[3], end="\n")
 
 
 def main():
-    new = read_file()
-    output_function(new)
+    text = read_file()
+    output_function(text)
 
 
 main()
