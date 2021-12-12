c8b138f9e40e3f17f69021a71a725e0b5ebd7fac
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-12 14:47:35 -0600

    aaaaaaaaaa

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -1,30 +1,36 @@
 # A thing
 
 from os import read
+import xml.etree.ElementTree as ET
 
 
 def read_file():
-    file = open("Final Project\plant_catalog.xml")
-    text = file.readlines()
-    file.close()
+    file = ET.parse("Final Project\plant_catalog.xml")
+    x = file.readlines()
+    text = x
     print(text)
     return text
 
 
 def process_text(text):
+    count = 0
     for index in range(len(text)):
         line = text[index].strip()
-        if index % 4 == 0:
+        if index % 10 == 0:
             common = line.split("<COMMON>")[0].split("</COMMON>")
-        elif index % 4 == 1:
+        elif index % 10 == 1:
             botanitcal = line.split("<BOTANICAL>")[0].split("</BOTANICAL>")[0]
-        elif index % 4 == 2:
-            zone = line.split("<ZONE>")[0].split("<?ZONE")[0]
-        elif index % 4 == 3:
+        elif index % 10 == 2:
+            zone = line.split("<ZONE>")[0].split("<?ZONE>")[0]
+        elif index % 10 == 3:
             light = line.split("<LIGHT>")[0].split("</LIGHT")[0]
-        elif index % 4 == 4:
+        elif index % 10 == 4:
+            count = count + 1
             price = line.split("<PRICE>")[0].split("</PRICE>")[0]
-            print(f"{common} ({botanitcal}) - {zone} - {light} - {price}")
+            print(f"{common} ({botanitcal}) - {zone} - {light} - {price}"
+            f"{price}")
+            print(f"{count} percent complete         \r")
+
 
 
 def main():
