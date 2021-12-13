e5c449a43f8a5c034322e50be06bb8bb590bea87
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-12 19:16:49 -0600

    y

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -12,6 +12,9 @@ def read_file():
             if os.stat("plant_catalog.xml").st_size > 0:
                 file = open("plant_catalog.xml")
                 text = file.readlines()
+                if text.count("  <PLANT>") < 0:
+                    print("No data")
+                    sys.exit()
             else:
                 print("No data")
                 sys.exit()
