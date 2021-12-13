3d9ea7d942b9bd2f4d63e9c3abbadddfcacaa2f9
j_samson3 <j_samson3@mail.harpercollege.edu>
2021-12-12 18:48:18 -0600

    small details

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -28,24 +28,24 @@ def process_text(text):
     count = 0
     total = 0
     for index in range(len(text)):
-            line = text[index].strip()
-            if index % 8 == 3:
-                common = line.split(">")[1].split("</")[0]
-            elif index % 8 == 4:
-                botanitcal = line.split(">")[1].split("</")[0]
-            elif index % 8 == 5:
-                zone = line.split(">")[1].split("</")[0]
-            elif index % 8 == 6:
-                light = line.split(">")[1].split("</")[0]
-            elif index % 8 == 7:
-                count = count + 1
-                try:
-                    price = float(line.split(">$")[1].split("</")[0])
-                except:
-                    print("missing data")
-                total = total + price
-                print(f"{common} ({botanitcal}) - {zone} - {light} {price}")
-                print(f"{count} items - ${round(total / count, 2)} average price\r")
+        line = text[index].strip()
+        if index % 8 == 3:
+            common = line.split(">")[1].split("</")[0]
+        elif index % 8 == 4:
+            botanitcal = line.split(">")[1].split("</")[0]
+        elif index % 8 == 5:
+            zone = line.split(">")[1].split("</")[0]
+        elif index % 8 == 6:
+            light = line.split(">")[1].split("</")[0]
+        elif index % 8 == 7:
+            count = count + 1
+            try:
+                price = float(line.split(">$")[1].split("</")[0])
+            except:
+                print("missing data")
+            total = total + price
+            print(f"{common} ({botanitcal}) - {zone} - {light} - ${price}")
+            print(f"{count} items - ${round(total / count, 2)} average price\r")
 
 
 def main():
